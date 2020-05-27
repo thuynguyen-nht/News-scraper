57654030de704bcfae205f344209b3c9e5402ffc
ThuyN <thuynguyen.nht20@gmail.com>
2020-05-26 20:58:08 -0700

    add save articles route,append to DOM

--- public/app.js
+++ public/app.js
@@ -23,20 +23,9 @@ $("#scraper").on("click", function () {
                     let clickTitle = $("<a target='_blank'>").attr("href", data[i].link);
                     let p = $("<p>").text(data[i].summary);
 
-                    // let btnArea = $("<div>");
-                    // let deleteBtnArea = $("<span>");
-                    // let deleteBtn = $("<i class='fas fa-trash-alt' id='delete-article'>  DELETE ARTICLES</i>")
-
-                    // let addBtnArea = $("<span>")
-                    // let addBtn = $("<i class='fas fa-sticky-note' id='access-notes' data-toggle='modal' data-target='#notes'>  NOTES</i>")
-
-                    // addBtnArea.append(addBtn);
-
-                    // deleteBtnArea.append(deleteBtn);
-                    // btnArea.append(addBtnArea, deleteBtnArea);
                     let btnArea = $("<div>");
                     let saveBtnArea = $("<span>");
-                    let saveBtn = $("<i class='fas fa-plus-circle' id='save-articles'>  Save Article</i>")
+                    let saveBtn = $("<i class='fas fa-plus-circle' id='save-articles'>  Save Article</i>").attr("data_id", data[i]._id);
                     saveBtnArea.append(saveBtn)
                     btnArea.append(saveBtnArea);
 
@@ -52,6 +41,49 @@ $("#scraper").on("click", function () {
         });
 });
 
-$("#save-articles").on("click", function () {
+$(document).on("click", "#save-articles", function () {
 
-})
+    var thisId = $(this).attr("data_id");
+    alert("Article saved. Article ID: " + thisId)
+    $.ajax({
+            method: "GET",
+            url: "/articles/saved/" + thisId,
+        })
+        // With that done
+        .then(function (data) {
+            // Log the response
+            console.log(data);
+            // Empty the notes section
+
+            let bigDiv = $("<div class='media my-3'>");
+
+            bigDiv.attr("data_id", data._id);
+
+            let smallDiv = $("<div class='media-body ml-3'>");
+            let clickImage = $("<a target='_blank'>").attr("href", data.link);
+            let image = $("<img alt='news' id='news-image'>").attr("src", data.image.split(" ")[0]);
+            let title = $("<h5 class='mt-0'>").text(data.title.replace(/\t/g, ''));
+            let clickTitle = $("<a target='_blank'>").attr("href", data.link);
+            let p = $("<p>").text(data.summary);
+
+            let btnArea = $("<div>");
+            let deleteBtnArea = $("<span>");
+            let deleteBtn = $("<i class='fas fa-trash-alt' id='delete-article'>  DELETE ARTICLES</i>")
+
+            let addBtnArea = $("<span>")
+            let addBtn = $("<i class='fas fa-sticky-note' id='access-notes' data-toggle='modal' data-target='#notes'>  NOTES</i>")
+
+            addBtnArea.append(addBtn);
+
+            deleteBtnArea.append(deleteBtn);
+            btnArea.append(addBtnArea, deleteBtnArea);
+
+            clickImage.append(image);
+            clickTitle.append(title)
+            smallDiv.append(clickTitle, p, btnArea)
+
+            bigDiv.append(clickImage, smallDiv);
+            $("#saved-section").append(bigDiv);
+
+        });
+});
