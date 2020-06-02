d800eef2bda99bfd29b8ad502db7f4ee665c9b5c
ThuyN <Thuynguyen.nht20@gmail.com>
2020-06-01 19:44:11 -0700

    add mongo connect

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.set("view engine", "handlebars");
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI, {
+mongoose.connect("mongodb://heroku_mxf6s813:m8gb7g0l8hfqj65lfuhcs6072j@ds119702.mlab.com:19702/heroku_mxf6s813", {
     useNewUrlParser: true
 });
 
