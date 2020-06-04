cce286b8efa58b787187556d5f6656a5ed4726ca
ThuyN <Thuynguyen.nht20@gmail.com>
2020-06-04 07:15:49 -0700

    working app with minor bug

--- public/app.js
+++ public/app.js
@@ -99,3 +99,53 @@ $(document).on("click", ".delete-article", function () {
             location.reload();
         });
 });
+
+//Go to the notes page for a particular article
+$(document).on("click", ".note-comment", function () {
+    var thisId = $(this).attr("data-id");
+    console.log(thisId);
+    $.ajax({
+        method: "GET",
+        url: "/articles/" + thisId
+    })
+    window.location.replace("/articles/" + thisId);
+});
+
+// Submit a note
+$(document).on("click", "#submit-note", function () {
+    // Grab the id associated with the article from the submit button
+    var thisId = $(this).attr("data-id");
+    // Run a POST request to save the note
+    $.ajax({
+        method: "POST",
+        url: "/articles/" + thisId,
+        data: {
+            // Value taken from title input
+            title: $("#title-note").val(),
+            // Value taken from note textarea
+            body: $("#note-description").val()
+        }
+    })
+        .then(function (data) {
+            // Log the response
+            console.log(data);
+            window.location.replace("/articles/" + data._id);
+        });
+    // Also, remove the values entered in the input and textarea for note entry
+    $("#title-note").val("");
+    $("#note-description").val("");
+});
+
+//delete a note
+$(document).on("click", ".delete-note", function () {
+    var thisId = $(this).attr("data-id");
+    $.ajax({
+        method: "DELETE",
+        url: "/articles/" + thisId
+    })
+        .then(function (data) {
+            // Log the response
+            console.log(data);
+            location.reload();
+        });
+});
