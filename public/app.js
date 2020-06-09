c489521a35ae933c5cc1056858b77ca525471c88
ThuyN <Thuynguyen.nht20@gmail.com>
2020-06-08 23:19:25 -0700

    take out commented code

--- public/app.js
+++ public/app.js
@@ -29,63 +29,6 @@ $(document).on("click", ".save-article", function () {
 
 });
 
-$(document).on("click", "#access-notes", function () {
-
-    $("#notes-display").empty();
-    // Save the id from the p tag
-    var thisId = $(this).attr("data_id");
-
-    // Now make an ajax call for the Article
-    $.ajax({
-        method: "GET",
-        url: "/articles/" + thisId
-    })
-        // With that done, add the note information to the page
-        .then(function (data) {
-            console.log(data);
-            // The title of the article
-            $(".modal-title").append("Notes for article ID: " + data._id);
-            $("#notes-display").append("<p id='bodyinput'></p>");
-            $("#form-notes").append("<button type='submit' class='btn btn-primary' id='submit' data_id='" + data._id + "'>Add notes</button>")
-
-
-            // If there's a note in the article
-            if (data.note) {
-
-                // Place the body of the note in the body textarea
-                $("#bodyinput").val(data.note.body);
-                $("#bodyinput").append("<span><button type='button' class='btn btn-danger'>X</button></span>")
-            }
-        });
-});
-
-$(document).on("click", "#submit", function (e) {
-    e.preventDefault();
-    // Grab the id associated with the article from the submit button
-    var thisId = $(this).attr("data-id");
-
-    // Run a POST request to change the note, using what's entered in the inputs
-    $.ajax({
-        method: "POST",
-        url: "/articles/" + thisId,
-        data: {
-
-            // Value taken from note textarea
-            body: $("#note-entry").val()
-        }
-    })
-        // With that done
-        .then(function (data) {
-            // Log the response
-            console.log(data);
-            // Empty the notes section
-            $("#notes-display").empty();
-        });
-
-    // Also, remove the values entered in the input for note entry
-
-    $("#note-entry").val("");
-});
 
 //Delete an article
 $(document).on("click", ".delete-article", function () {
@@ -101,7 +44,7 @@ $(document).on("click", ".delete-article", function () {
         });
 });
 
-console.log("hello");
+
 //Go to the notes page for a particular article
 $(document).on("click", ".note-comment", function () {
     var thisId = $(this).attr("data-id");
@@ -109,10 +52,11 @@ $(document).on("click", ".note-comment", function () {
     $.ajax({
         method: "GET",
         url: "/articles/" + thisId
-    }).then(function (data) {
-        console.log(data);
-        window.location.replace("/articles/" + thisId);
     })
+        .then(function (data) {
+            console.log(data);
+            window.location.replace("/articles/" + thisId);
+        })
 
 });
 
